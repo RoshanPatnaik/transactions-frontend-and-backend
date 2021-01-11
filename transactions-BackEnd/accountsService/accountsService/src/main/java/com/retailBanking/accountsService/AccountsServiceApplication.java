a11355a8adf2ece548141832df4e2fde52a43f69
loser968 <mproshan968@gmail.com>
2021-01-11 12:27:26 +0530

    updated project

---
+++ transactions-BackEnd/accountsService/accountsService/src/main/java/com/retailBanking/accountsService/AccountsServiceApplication.java
@@ -0,0 +1,19 @@
+package com.retailBanking.accountsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableFeignClients("com.retailBanking.accountsService")
+public class AccountsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsServiceApplication.class, args);
+	}
+
+}
